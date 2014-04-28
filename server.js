9bcebd6ae51ba3e445326f1e31feff33a9744644
Steven Zeiler <zeiler.steven@gmail.com>
2014-04-28 05:17:51 -0700

    [FEATURE] Add basic root route.

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ var util = require('util');
 var app = express();
 
 app.post('/payments/confirmations', function(req, res){
-
   util.puts(req.body);
+});
 
+app.get('/', function(req, res){
+  res.send('Astropay-Ripple');  
 });
 
 app.listen();
