4feb73822f9a051e22271df661d95363c27725d9
Steven Zeiler <zeiler.steven@gmail.com>
2014-04-28 05:24:51 -0700

    [CHORE] Listen on PORT not port.

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 var express = require('express');
-var util = require('util');
 
 var app = express();
 
 app.post('/payments/confirmations', function(req, res){
-  util.puts(req.body);
+  console.log(req.body);
 });
 
 app.get('/', function(req, res){
   res.send('Astropay-Ripple');  
 });
 
-app.listen(process.env.port || 7777);
+app.listen(process.env.PORT || 7777);
 
