1b85b8a2fbf6df8827536a0259d43f42d64621f6
Steven Zeiler <zeiler.steven@gmail.com>
2014-04-28 05:39:32 -0700

    [FEATURE] Add return post url.

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.post('/payments/confirmations', function(req, res){
   console.log(req.body);
 });
 
+app.post('/payments/returns', function(req, res){
+  console.log(req.body);
+});
+
 app.get('/', function(req, res){
   res.send('Astropay-Ripple');  
 });
