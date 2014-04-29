651694ac889439a7366af584a9da89646b3db1ba
Steven Zeiler <zeiler.steven@gmail.com>
2014-04-29 13:37:29 -0700

    [FEATURE] Post to ripple gateway upon callback.

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var request = require('request');
 
 var app = express();
 app.use(bodyParser());
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 app.post('/payments/confirmations', function(req, res){
-  console.log(req.body);
+  try {
+    request.post('https://75.101.201.120/v1/deposits', { form: {
+      currency: req.body.x_currency,
+      amount: req.body.x_amount,
+      externalAccountId: req.body.x_iduser,
+      data: req.body
+    }}, function(err, resp) {
+      console.log(err, resp);
+    });
+  } catch(e) {
+    console.log('error', e);
+  }
 });
 
-app.post('/payments/returns', function(req, res){
+app.post('/', function(req, res){
   console.log(req.body);
-});
-
-app.get('/', function(req, res){
-  res.send('Astropay-Ripple');  
+  res.send('Astropay-Ripple payment complete!!');  
 });
 
 app.listen(process.env.PORT || 7777);
