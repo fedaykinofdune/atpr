321b6df0143f693e1e868a4885df699e3a785aa4
Steven Zeiler <zeiler.steven@gmail.com>
2014-04-29 13:51:12 -0700

    [FEATURE] Add content-type.

--- server.js
+++ server.js
@@ -8,20 +8,25 @@ app.use(bodyParser());
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 app.post('/payments/confirmations', function(req, res){
-  console.log(req.body);
-  console.log('about to post to the gateway');
-  request.post('https://75.101.201.120/v1/deposits', { form: {
+  var deposit = {
     currency: req.body.x_currency,
     amount: req.body.x_amount,
     externalAccountId: req.body.x_iduser,
     data: req.body
-  }}, function(err, resp) {
+  };
+  console.log('about to deposit', deposit);
+  request.post({
+    headers: {'content-type': 'application/json' },
+    url: 'https://75.101.201.120/v1/deposits',
+    body: deposit
+  }, function(err, resp) {
+    console.log('post finished');
+    console.log(err, resp);
     if (err){
       res.send(500);
     } else {
       res.send(200);
     }
-    console.log(err, resp);
   });
 });
 
